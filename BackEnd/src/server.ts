2d5191edb312b2761746b9c6c89a5b0a18a73185
sand1no1 <a00838292@tec.mx>
2025-04-06 21:22:04 -0600

    feat: registrar rutas de usuario, autenticación y equipos en el servidor

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/user";
+import signupRoutes from "./routes/user.routes";
+import auth0Routes from "./routes/auth0.routes";
+import teamRoutes from "./routes/team.routes";
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 
-app.use("/api", userRoutes);
+app.use("/api", signupRoutes);
+app.use("/api", auth0Routes);
+app.use("/api", teamRoutes);
 
 app.listen(4000, () => {
   console.log("Server running at PORT 4000");
