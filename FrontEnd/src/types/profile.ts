80f6a414971f736baba9b19849c6bd6b0735b6a1
sand1no1 <a00838292@tec.mx>
2025-04-06 21:11:45 -0600

    feat: definir tipo ProfileData para estructurar datos del perfil de usuario

---
+++ FrontEnd/src/types/profile.ts
@@ -0,0 +1,10 @@
+export interface ProfileData {
+    name: string;
+    lastName: string;
+    position: string;
+    email: string;
+    phone: string;
+    dateOfJoining: string;
+    education: string;
+    profilePic: string | null;
+}
