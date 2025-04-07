06a9a18a6fb6dd1a9e842b07f632e6e2cfb8d913
sand1no1 <a00838292@tec.mx>
2025-04-06 21:15:34 -0600

    feat: ampliar configuración de Auth0 con nuevos parámetros para login y gestión

--- src/config/auth0.ts
+++ src/config/auth0.ts
@@ -5,4 +5,8 @@ export const auth0Config = {
   domain: process.env.AUTH0_DOMAIN!,
   clientId: process.env.AUTH0_CLIENT_ID!,
   clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+  audience: process.env.AUTH0_AUDIENCE!,
+  clientIdFront: process.env.AUTH0_CLIENT_ID_FRONT!,
+  clientSecretFront: process.env.AUTH0_CLIENT_SECRET_FRONT!,
+  audienceManagement: process.env.AUTH0_AUDIENCE_MANAGEMENT!
 };
