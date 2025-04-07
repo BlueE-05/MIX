6ceff550ff5cb3a6b894e26b7cb31ee15b280b87
sand1no1 <a00838292@tec.mx>
2025-04-06 21:11:04 -0600

    feat: crear hook useProfile para obtener datos del usuario desde la API

---
+++ FrontEnd/src/hooks/useProfile.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ProfileData } from "@/types/profile"
+
+
+export function useProfile(accessToken: string) {
+    const [profile, setProfile] = useState<ProfileData | null>(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      if (!accessToken) return;
+  
+      fetch("http://localhost:4000/api/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setProfile({
+            name: data.Name,
+            lastName: data.LastName,
+            email: data.Email,
+            phone: data.PhoneNumber,
+            education: data.Education,
+            dateOfJoining: new Date(data.JoiningDate).toLocaleDateString(),
+            position: data.JobPosition || "N/A",
+            profilePic: data.ProfilePic || null,
+          });
+          setLoading(false);
+        })
+        .catch(() => setLoading(false));
+    }, [accessToken]);
+  
+    return { profile, loading };
+  }
