a319cf3375c85630f3d6e443570e7cda9709bba3
sand1no1 <a00838292@tec.mx>
2025-04-06 21:23:12 -0600

    feat: extender tipos de Auth0 para manejo de JWT y autenticación en requests

--- src/types/auth0.ts
+++ src/types/auth0.ts
@@ -1,4 +1,22 @@
+import { Request } from "express";
+
 export interface Auth0User {
     email_verified: boolean;
 }
-  
+
+export interface CustomAuthClaims {
+    sub: string;
+    email: string;
+    email_verified: boolean;
+    iat?: number;
+    exp?: number;
+    [key: string]: any;
+}
+
+export interface AuthRequest extends Request {
+    auth: {
+      sub: string;
+      email?: string;
+      [key: string]: any;
+    };
+}
