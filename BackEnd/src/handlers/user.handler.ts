54e8cbdedf161f2d7e81fb987f7e09d3def326ab
sand1no1 <a00838292@tec.mx>
2025-04-06 21:05:34 -0600

    feat: agregar handler HTTP para registro y obtención de perfil de usuario

---
+++ BackEnd/src/handlers/user.handler.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { UserController } from "../controllers/user.controller";
+import { auth0Service } from "../services";
+import { pool } from "../db";
+import { UserDbService } from "../db/user";
+
+const controllerPromise = pool.then(
+  (db) => new UserController(auth0Service, new UserDbService(db))
+);
+
+export class UserHttpHandler {
+  async signup(req: Request, res: Response, next: NextFunction) {
+    try {
+      const controller = await controllerPromise;
+      await controller.signup(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getProfile(req: Request, res: Response, next: NextFunction) {
+    try {
+      const controller = await controllerPromise;
+      await controller.getProfile(req as any, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
