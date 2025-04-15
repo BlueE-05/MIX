797779fb5a778661fd31777a8085aa40f30af2f3
sand1no1 <a00838292@tec.mx>
2025-04-15 01:38:05 -0600

    style: unificación de métodos que utilizan /api en su inicio del endpoint

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -2,16 +2,14 @@ import express from "express";
 import cors from "cors";
 import signupRoutes from "./routes/user.routes";
 import auth0Routes from "./routes/auth0.routes";
-import teamRoutes from "./routes/team.routes";
+import teamRoutes from "./routes/job.routes";
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 
-app.use("/api", signupRoutes);
-app.use("/api", auth0Routes);
-app.use("/api", teamRoutes);
+app.use("/api", signupRoutes, auth0Routes, teamRoutes);
 
 app.listen(4000, () => {
   console.log("Server running at PORT 4000");
