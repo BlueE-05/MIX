9faa7cffeb3d3cdad55a5937582b3a6583362691
sand1no1 <a00838292@tec.mx>
2025-04-15 02:14:42 -0600

    fix: correccion para uso de email como ID e implementación de IDJobPosition para busqueda de puesto

--- FrontEnd/src/types/signup.ts
+++ FrontEnd/src/types/signup.ts
@@ -1,16 +1,15 @@
 export interface SignupFormProps {
-    onSubmit: (event: React.FormEvent, formData: SignupFormData) => void;
-  }
-  
+  onSubmit: (event: React.FormEvent, formData: SignupFormData) => void;
+}
+
 export interface SignupFormData {
-    Name: string;
-    LastName: string;
-    Email: string;
-    Password: string;
-    PhoneNumber: string;
-    BirthDate: string;
-    Education: string;
-    ProfilePic: string | null;
-    IdTeam: string;
-    JobPosition: string;
+  Name: string;
+  LastName: string;
+  Email: string;
+  Password: string;
+  PhoneNumber: string;
+  BirthDate: string;
+  Education: string;
+  ProfilePic: string | null;
+  IDJobPosition: string;
 }
