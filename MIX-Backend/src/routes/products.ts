662b802ce677b5d4984b06e76f7a372762cc22a1
BlueE-05 <danaetorres35@gmail.com>
2025-04-21 21:01:06 -0600

    product update

--- MIX-Backend/src/routes/products.ts
+++ MIX-Backend/src/routes/products.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+<<<<<<< Updated upstream
 import ProductHandler from '@/handlers/products';
 
 const router = express.Router();
@@ -11,5 +12,26 @@ router.get('/name/:name', productHandler.getProductByName);
 router.post('/', productHandler.createProduct);
 router.put('/:id', productHandler.updateProduct);
 router.delete('/:id', productHandler.deleteProduct);
+=======
+import ProductHTTPHandler from '@/handlers/products';
+
+const router = express.Router();
+const productHandler = new ProductHTTPHandler();
+
+router.get('/', productHandler.getProducts);
+
+router.get('/:id', productHandler.getProductById);
+
+router.post('/:id', productHandler.createContact);
+
+router.put('/:id', productHandler.updateProduct);
+
+router.delete(':id', productHandler.deleteProduct);
+
+router.get('/', productHandler.productById);
+
+router.get('/', productHandler.productByName);
+
+>>>>>>> Stashed changes
 
 export default router;
