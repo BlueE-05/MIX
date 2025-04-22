45b32638c0ff7a46fba7789b1c837f42b370b852
Maximo Ramirez <a00839731@tec.mx>
2025-04-22 08:49:35 -0600

    Siii se termino la clase, pero ya casi tenemos que presentar el proyecto ;-;

--- MIX-Frontend/src/app/crm/information/page.tsx
+++ MIX-Frontend/src/app/crm/information/page.tsx
@@ -1,7 +1,38 @@
+'use client'
+import { useRef, useEffect } from 'react';
+
 export default function InfoPage() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    if (iframeRef.current) {
+      iframeRef.current.src = "https://www.youtube.com/embed/WCAFgCcPOI4?autoplay=1&mute=0";
+    }
+  }, []);
+
   return (
     <div className="flex gap-4 p-8">
-      Info thingies
+      <div className="w-full max-w-2xl">
+        <iframe
+          ref={iframeRef}
+          width="100%"
+          height="400"
+          src="https://www.youtube.com/embed/WCAFgCcPOI4?autoplay=1&mute=0"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
+
+/*
+export default function InfoPage() {
+  return (
+    <div className="flex gap-4 p-8">
+     
+    </div>
+  );
+}
+*/
