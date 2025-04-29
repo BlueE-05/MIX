6742aa2e71b1e46d26c0f7664da8bf9322936f4c
BlueE-05 <danaetorres35@gmail.com>
2025-04-29 12:51:17 -0600

    front sales

--- MIX-Frontend/src/app/crm/sales/page.tsx
+++ MIX-Frontend/src/app/crm/sales/page.tsx
@@ -194,6 +194,7 @@ export default function SalesPage() {
               startDate: null,   
             };
             handleNewSale(adaptedData);
+            fetchSales();
           }}
         />
       )}
